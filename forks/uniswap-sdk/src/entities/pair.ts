cf29970218d4bccb91568bc11095fd2a6493fc4c
Chef Nomi <defichefnomi@gmail.com>
2020-09-10 01:52:23 +0800

    s/Uniswap V2/SushiSwap LP Token

--- forks/uniswap-sdk/src/entities/pair.ts
+++ forks/uniswap-sdk/src/entities/pair.ts
@@ -56,7 +56,7 @@ export class Pair {
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
       'UNI-V2',
-      'Uniswap V2'
+      'SushiSwap LP Token'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
--- forks/uniswap-sdk/src/router.ts
+++ forks/uniswap-sdk/src/router.ts
@@ -29,11 +29,11 @@ export interface TradeOptions {
 }
 
 /**
- * The parameters to use in the call to the Uniswap V2 Router to execute a trade.
+ * The parameters to use in the call to the SushiSwap LP Token Router to execute a trade.
  */
 export interface SwapParameters {
   /**
-   * The method to call on the Uniswap V2 Router.
+   * The method to call on the SushiSwap LP Token Router.
    */
   methodName: string
   /**
@@ -53,7 +53,7 @@ function toHex(currencyAmount: CurrencyAmount) {
 const ZERO_HEX = '0x0'
 
 /**
- * Represents the Uniswap V2 Router, and has static methods for helping execute trades.
+ * Represents the SushiSwap LP Token Router, and has static methods for helping execute trades.
  */
 export abstract class Router {
   /**
